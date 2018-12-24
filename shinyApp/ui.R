dc05fd8d92a399632c5a9cdb5fbb65a59b72af12
ludovik tekam <ludovik.tekam@edu.univ-paris13.fr>
2018-12-24 10:23:45 +0100

    nav bar ok

--- shinyApp/ui.R
+++ shinyApp/ui.R
@@ -1,26 +1,59 @@
 library(shiny)
 
 shinyUI(
-  fluidPage(
-
-    titlePanel(title = " Connexion "),
-
-
-
-    sidebarLayout(
-      sidebarPanel(
-        textInput("login","Login",placeholder="login..."),
-        passwordInput("pwd","Password",placeholder="passeword..."),
-        submitButton("Connexion")
-      ),
-
-      mainPanel(
-
-        textOutput("outlogin"),
-        textOutput("outpwd"),
-        tableOutput("tab")
-      )
-    )
-  )
-
-)
+    navbarPage(
+      title = "Intergromics PFA",
+      tabPanel("Connexion",
+               fluidPage(
+                 titlePanel(title = " Connexion "),
+                 sidebarLayout(
+                   sidebarPanel(
+                     textInput("login","Login",placeholder="login..."),
+                     passwordInput("pwd","Password",placeholder="passeword..."),
+                     submitButton("Connexion")
+                   ),#end  sidebarPanel
+                   mainPanel(
+                     textOutput("outlogin"),
+                     textOutput("outpwd"),
+                     tableOutput("tab")
+                   )#end mainPanel
+                   
+                 )#end sidebarLayout
+               )#end fluidPage
+               
+               ),#end tabPanel 
+      
+      tabPanel("import Doc",
+               sidebarLayout(
+                 sidebarPanel(
+                   sliderInput("b", "Select no. of BINs", min = 5, max = 20,value = 10)
+                 ),#end  sidebarPanel
+                 mainPanel(
+                   plotOutput("plot")
+                 )#end mainPanel
+               )
+      ),#end tabPanel
+      
+      tabPanel("recherche",
+               sidebarLayout(
+                 sidebarPanel(
+                   sliderInput("b", "Select no. of BINs", min = 5, max = 20,value = 10)
+                 ),#end  sidebarPanel
+                 mainPanel(
+                   plotOutput("plot")
+                 )#end mainPanel
+               )
+      ),#end tabPanel
+      
+      navbarMenu("Analyse Options",
+                 tabPanel("Menu Analyse A - Summary stats", verbatimTextOutput("summary")),
+                 tabPanel("Menu Analyse B - Link to code",
+                          h4(HTML(paste("Thanks for watching the video. Reference code available at the following", a(href="https://github.com/aagarw30/R-Shinyapp-Tutorial/tree/master/shinylayouts/navbarpage%20demo", "link"), "."))),
+                          h4(HTML(paste("In case you have questions", a(href="mailto:tekamludovik23@gmail.com", "email me"), ".")))
+                          
+                 ))
+    )#end navbarPage
+  
+  
+  
+)#end shinyUI
